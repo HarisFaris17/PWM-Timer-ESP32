5d3b55063117efda59e3724cf623f957cede4619
HarisFaris17 <harisfaris17@gmail.com>
2022-09-12 08:14:45 +0700

    provision timer, attach the timer to interrupt, make led pin output, hence when the interrupts evoked, the led will blink

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
-#include <Arduino.h>
-
-void setup() {
-  // put your setup code here, to run once:
+#include<Arduino.h>
+#define LED 14
+hw_timer_t *My_timer = NULL;
+boolean change = true;
+void IRAM_ATTR onTimer(){
+  digitalWrite(LED, !digitalRead(LED));
+  timerDetachInterrupt(My_timer);
+  if(change){
+    timerAttachInterrupt(My_timer, &onTimer, true);
+    timerAlarmWrite(My_timer, 6000000, true);
+    timerAlarmEnable(My_timer); //Just Enable
+    change=false;
+  }else{
+    timerAttachInterrupt(My_timer, &onTimer, true);
+    timerAlarmWrite(My_timer, 1000000, true);
+    timerAlarmEnable(My_timer); //Just Enable
+    change=true;
+  }
 }
-
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED, OUTPUT);
+  My_timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(My_timer, &onTimer, true);
+  timerAlarmWrite(My_timer, 1000000, true);
+  timerAlarmEnable(My_timer); //Just Enable
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(10000);
+  Serial.println("test");
 }
